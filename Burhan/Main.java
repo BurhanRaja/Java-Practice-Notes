977d60dc346c41d138d4506be55fc1b5a632addd
BurhanuddinRaja <mails4burhan@gmail.com>
2022-04-04 16:06:27 +0530

    Explained code in Main file added

---
+++ Burhan/Main.java
@@ -0,0 +1,36 @@
+package Burhan;
+
+public class Main{
+    public static void main(String[] args){
+
+        // System.out.println("Hello World!");
+        
+        System.out.print("Hello World! ");
+        System.out.println("Myself Burhan.");
+    }
+}
+
+// ********* Sturcture and Meaning *************
+
+// 'package' is the folder in which your java file lies.
+
+// 'public' is written, so that this class can be used anywhere.
+
+// 'class' is the collection or group of functions.
+// In class the 'functions' are called methods.
+
+// The 'classname' should be equivalent to the file name. 
+
+// The 'public' in second line is for the usability that can be used in any methods.
+
+// 'main' means this function should run first. The program starts from here.
+
+// 'static' means we have to run the program without creating the object of the class.
+
+// 'void' determines the type of value of the return value.
+// Here, we do not want any type of value, so it is void.
+
+// 'String[] args' means an argument that stores all the print output in an array.
+
+// In 'println', 'print' is for printing the object and 'ln' is to begin from the new line.'
+
