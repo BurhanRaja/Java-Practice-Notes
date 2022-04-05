c55aaa0b727b9986fe8fc38ab0f0928bb1dcb95c
BurhanuddinRaja <mails4burhan@gmail.com>
2022-04-05 17:52:21 +0530

    Added files in conditions and loops

--- Conditions_and_Loops/src/com/Burhan/For_Loop.java
+++ Conditions_and_Loops/src/com/Burhan/For_Loop.java
@@ -1,7 +1,30 @@
 package com.Burhan;
 
+import java.util.Scanner;
+
 public class For_Loop {
     public static void main(String[] args) {
         
+        // Print number 1 to 5
+        // for (int i = 1; i <= 5; i++) {
+        //     System.out.println(i);
+        // }
+
+        // Print n numbers
+        Scanner inp = new Scanner(System.in);
+
+        int n = inp.nextInt();
+
+        // To print the numbers
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " ");
+        }
+
+        // To print Hello World
+        for (int i = 1; i <= n; i++) {
+            System.out.print("Hello World ");
+        }
+
+        inp.close();
     }
 }
---
+++ Conditions_and_Loops/src/com/Burhan/While_Loop.java
@@ -0,0 +1,7 @@
+package com.Burhan;
+
+public class While_Loop {
+    public static void main(String[] args) {
+        
+    }
+}
