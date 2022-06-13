d1f6f4b0dd394f0e5a767c05eb015e5da0c8e29e
BurhanuddinRaja <mails4burhan@gmail.com>
2022-06-13 09:41:34 +0530

    class and objects created

--- OOPs/src/com/Burhan/Example_Student.java
+++ OOPs/src/com/Burhan/Example_Student.java
@@ -1,7 +1,5 @@
 package com.Burhan;
 
-import java.util.Arrays;
-
 public class Example_Student {
     public static void main(String[] args) {
         Student std1 = new Student();
@@ -10,9 +8,12 @@ public class Example_Student {
         std1.marks = 89.5f;
         System.out.println(std1);
 
-        // Student[] students = new Student[5];
-
-        // System.out.println(Arrays.toString(students));
+        // Student is a class instantialize.
+        // std1 is an object of a student.
+        // rno, name and marks are reference variable for objects to access.
+        // new is to create new object.
+        // A constructor defines what will happen when your object is created. It is also a special function.
+        // Student() is a default constructor, already present in the class. 
     }
 }
 
