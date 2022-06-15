e9460db35234e5ca2b235b5b6e385e5469e3142a
BurhanuddinRaja <mails4burhan@gmail.com>
2022-06-15 17:34:19 +0530

    access modifiers added

--- OOPs/bank/Account.java
+++ OOPs/bank/Account.java
@@ -4,4 +4,28 @@ public class Account {
     public void call() {
         System.out.println("Package has been called");
     }
+
+    // Access Modifiers
+    public String name; // Can be accessed by any class
+    protected String email; // Can be accessed by class within the package file
+    private long accNum; // Can be accessed by only Account class
+
+    // getters
+    public long getAccNum() {
+        return this.accNum;
+    }
+    public void setAccNum(long accountNumber) {
+        this.accNum = accountNumber;
+    }
+}
+
+// How to access private modifiers?
+// getters and setters
+
+class GetandSet {
+    public static void main(String[] args) {
+        Account newAcc = new Account();
+        newAcc.setAccNum(13090921121L);
+        System.out.println(newAcc.getAccNum());
+    }
 }
